ec242dcd1ae966c11b5fd7a41b9303f07efaf184
griefless <craymbau@gmail.com>
2026-05-04 00:21:50 +0200

    Add Facebook SDK initialization in main.jsx
    Initialized Facebook SDK for app integration.

--- pageboost-netlify-ai 3/src/main.jsx
+++ pageboost-netlify-ai 3/src/main.jsx
@@ -2,6 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// ✅ FACEBOOK SDK INITIALIZATION
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '1294905135361425',
+    cookie     : true,
+    xfbml      : true,
+    version    : 'v18.0'
+  });
+};
+
+// Load the SDK asynchronously
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {return;}
+  js = d.createElement(s); js.id = id;
+  js.src = "https://connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+// ✅ END FACEBOOK SDK
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />

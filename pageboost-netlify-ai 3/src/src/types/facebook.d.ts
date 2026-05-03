9f915580d4c7fc008d8caf38cd1885b7c61ace88
griefless <craymbau@gmail.com>
2026-05-04 00:14:26 +0200

    Add TypeScript definitions for Facebook SDK

---
+++ pageboost-netlify-ai 3/src/src/types/facebook.d.ts
@@ -0,0 +1,13 @@
+// src/types/facebook.d.ts
+export {};
+
+declare global {
+  interface Window {
+    FB: {
+      init: (config: { appId: string; cookie: boolean; xfbml: boolean; version: string }) => void;
+      login: (callback: (response: any) => void, options?: { scope: string; enable_profile_selector?: boolean }) => void;
+      getLoginStatus: (callback: (response: any) => void) => void;
+      api: (path: string, params?: any, callback?: (response: any) => void) => void;
+    };
+  }
+}
